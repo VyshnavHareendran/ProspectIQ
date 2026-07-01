720345f9d1025ce93f69fb72348c8d829e97e4c7
Vyshnav Hareendran <vyshnavofficial21@gmail.com>
2026-07-01 21:34:31 +0530

    Project frontend structure

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,10 +3,21 @@ from fastapi import FastAPI
 from app.routes.auth import router as auth_router
 from app.routes.business import router as business_router
 
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="ProspectIQ",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth_router)
